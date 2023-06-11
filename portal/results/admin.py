973e7c7dca9db9418211795acea5aa996258bed4
elinteerie <elinteerie@gmail.com>
2023-06-11 09:58:48 +0100

    Added Course Item
    Added CourseItem to Populate Results and Grades as many as

--- portal/results/admin.py
+++ portal/results/admin.py
@@ -3,11 +3,23 @@ from . import models
 # Register your models here.
 
 
+class CourseItemInline(admin.TabularInline):
+    model = models.CourseItem
+    raw_id_fields = ['student']
+
+
+
 admin.site.register(models.Faculty)
 admin.site.register(models.Department)
 admin.site.register(models.Semester)
 admin.site.register(models.Student)
-admin.site.register(models.Course)
+
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'course_code']
+
+    inlines = [CourseItemInline]
 
 
 
--- portal/results/models.py
+++ portal/results/models.py
@@ -48,6 +48,21 @@ class Course(models.Model):
         super().save(*args, **kwargs)
 
 
+class CourseItem(models.Model):
+    course = models.ForeignKey(Course, related_name='course', on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, related_name='student', on_delete=models.CASCADE)
+    student_course_ca = models.IntegerField()
+    student_course_exam_score = models.IntegerField()
+    student_grade = models.CharField(max_length=1, blank=True)
 
+    def save(self, *args, **kwargs):
+        if self.student_course_ca + self.student_course_exam_score >=70:
+            self.student_grade = "A"
+        elif self.student_course_ca + self.student_course_exam_score >=69:
+            self.student_grade = "B"
+        elif self.student_course_ca + self.student_course_exam_score >=59:
+            self.student_grade ="C"
 
-
+        else:
+            self.grade = "D"
+        super().save(*args, **kwargs)
